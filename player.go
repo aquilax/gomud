c40ffb49eaf7d8d3f8366849edc3776d74717ce0
Evgeni Vasilev <aquilax@gmail.com>
2012-02-26 12:16:59 +0200

    Added more Player functions

--- player.go
+++ player.go
@@ -45,6 +45,32 @@ type Player struct {
 	m_newbie bool
 }
 
+func NewPlayer() (*Player) {
+	p := &Player{
+		m_pass: "UNDEFINED",
+		m_rank: REGULAR,
+		m_loggedin: false,
+		m_active: false,
+		m_newbie: true,
+		m_experience: 0,
+		m_level: 1,
+		m_room: 0,
+		m_money: 0,
+		m_baseattributes: AttributeSet{
+			STRENGTH: 1,
+			HEALTH: 1,
+			AGILITY: 1,
+		},
+		m_statpoints: 18,
+		m_items: 0,
+		m_weapon: -1,
+		m_armor: -1,
+	}
+	p.RecalculateStats()
+	p.m_hitpoints = p.GetAttr(MAXHITPOINTS)
+	return p
+}
+
 func (p *Player) NeedForLevel(p_level int) (int) {
 	return (int)(math.Pow(1.4, float64(p_level))-1)
 }
@@ -140,3 +166,92 @@ func (p *Player) AddDynamicBonuses (p_item *Item) {
 		p.m_attributes[i] += p_item.GetAttr(i);
 	}
 }
+
+func (p *Player) AddBonuses(p_item *Item) {
+	if (p_item == nil) {
+		return
+	}
+	for i := 0; i < NUMATTRIBUTES; i++ {
+		p.m_baseattributes[i] += p_item.GetAttr(i);
+	}
+	p.RecalculateStats()
+}
+
+func (p *Player) PickUpItem(p_item *Item) (bool) {
+	if p.m_items < PLAYERITEMS {
+		for i := 0; i < PLAYERITEMS; i++ {
+			if p.m_inventory[i] == nil {
+				p.m_inventory[i] = p_item
+				p.m_items++
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (p *Player) DropItem(p_index int) (bool) {
+	if p.m_inventory[p_index] != nil {
+		if p.m_weapon == p_index {
+			p.RemoveWeapon()
+		}
+		if p.m_armor == p_index {
+			p.RemoveArmor()
+		}
+		p.m_inventory[p_index] = nil
+		p.m_items--
+		return true
+	}
+	return false
+}
+
+func (p *Player) RemoveWeapon() {
+	p.m_weapon = -1
+	p.RecalculateStats()
+}
+
+func (p *Player) UseWeapon(p_index int) {
+	p.RemoveWeapon()
+	p.m_weapon = p_index
+	p.RecalculateStats()
+}
+
+func (p *Player) RemoveArmor() {
+	p.m_armor = -1
+	p.RecalculateStats()
+}
+
+func (p *Player) UseArmor(p_index int) {
+	p.RemoveArmor()
+	p.m_armor = p_index
+	p.RecalculateStats()
+}
+
+func (p *Player) SendString(p_string string) {
+	if p.m_request.conn == nil {
+		//LOG
+		return
+	}
+	p.m_request.conn.Write([]byte(p_string+newline))
+	if p.m_active {
+		p.PrintStatbar(false)
+	}
+}
+
+func (p *Player) PrintStatbar(p_update bool) {
+    if p_update {
+        return
+	}
+    statbar := white + bold + "["
+    ratio := int(100 * p.m_hitpoints / p.GetAttr(MAXHITPOINTS))
+    if ratio < 33 {
+        statbar += red
+	} else if ratio < 67 {
+        statbar += yellow
+    } else {
+        statbar += green
+	}
+    statbar += string(p.m_hitpoints) + white + "/" + string(p.GetAttr(MAXHITPOINTS)) + "] "
+	p.m_request.conn.Write([]byte(clearline + statbar + reset))
+}
+
